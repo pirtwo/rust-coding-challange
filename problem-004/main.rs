73dd9fdbcf6676ab476cb44b3300b23984d30737
Ahmad Fz <ahmad.f.zadeh@icloud.com>
2020-08-01 19:47:11 +0430

    Create main.rs

---
+++ problem-004/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let res = palindrome();
+    print!(
+        "The bigest palindrome number is {} * {} = {}",
+        res.0, res.1, res.2
+    );
+}
+
+// return largest palindrome number
+// of two 3-digit number multiplition
+fn palindrome() -> (u32, u32, u32) {
+    for i in (100..=999).rev() {
+        for j in (100..=999).rev() {
+            let num = (i * j).to_string();
+            let rev = num.chars().rev().collect::<String>();
+            if num == rev {
+                return (i, j, i * j);
+            }
+        }
+    }
+
+    return (0, 0, 0);
+}
