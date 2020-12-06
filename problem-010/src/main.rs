bc2285ccfdacc992e0e2825e5cbe55af0eae3c9f
Ahmad Fz <ahmad@pop-os.localdomain>
2020-12-06 12:47:55 +0330

    add problem-010

---
+++ problem-010/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let primes = eratosthenes_sieve(2000000);
+
+    let sum: u64 = primes.iter().map(|x| x.value).sum();
+
+    println!("sum of primes below 2 Milion is = {}", sum);
+}
+
+fn eratosthenes_sieve(up_to: u64) -> Vec<Number> {
+    let mut numbers = vec![];
+
+    for i in 2..=up_to {
+        numbers.push(Number::new(i, true));
+    }
+
+    let upper_bound = ((up_to - 2) as f64).sqrt().floor() as usize;
+
+    for i in 0..=upper_bound {
+        if numbers[i].is_prime {
+            let prime = numbers[i].value;
+            let mut idx = prime.pow(2) - 2;
+            loop {
+                if idx < numbers.len() as u64 {
+                    numbers[idx as usize].is_prime = false;
+                    idx += prime;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    numbers.retain(|x| x.is_prime);
+    numbers
+}
+
+#[derive(Debug)]
+struct Number {
+    value: u64,
+    is_prime: bool,
+}
+
+impl Number {
+    fn new(value: u64, is_prime: bool) -> Self {
+        Number { value, is_prime }
+    }
+}
